a318366cc2780badf86a43b434a388cd837b3aa2
nonseww <innapolituchaya@gmail.com>
2026-05-02 06:49:57 +0400

    make the window draggable

--- src/components/SmallWidget/SmallWidget.tsx
+++ src/components/SmallWidget/SmallWidget.tsx
@@ -1,17 +1,46 @@
 import Logo from "../../assets/logo60x60.jpg";
 import classes from "./SmallWidget.module.scss";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import { useRef } from "react";
 
 interface Props {
   onClick: () => void;
 }
 
 export const SmallWidget = ({ onClick }: Props) => {
+  const startPos = useRef<{ x: number; y: number } | null>(null);
+
+  const handleMouseDown = async (e: React.MouseEvent) => {
+    if (e.button === 0) {
+      startPos.current = { x: e.screenX, y: e.screenY };
+    }
+  };
+
+  const handleMouseMove = async (e: React.MouseEvent) => {
+    if (startPos.current) {
+      const deltaX = Math.abs(startPos.current.x - e.screenX);
+      const deltaY = Math.abs(startPos.current.y - e.screenY);
+
+      if (deltaX > 5 || deltaY > 5) {
+        startPos.current = null;
+        await getCurrentWindow().startDragging();
+      }
+    }
+  };
+
+  const handleMouseUp = () => {
+    startPos.current = null;
+  };
+
   return (
-    <img
-      src={Logo}
-      className={classes.logo}
+    <div
+      className={classes.container}
       onClick={onClick}
-      style={{ width: "100%", height: "100%", objectFit: "cover" }}
-    />
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    >
+      <img src={Logo} className={classes.logo} />
+    </div>
   );
 };
